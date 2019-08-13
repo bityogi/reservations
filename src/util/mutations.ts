3167bc72c17150ff43621cdd8950110f62bf85da
Prem Govinda <govindaagain@gmail.com>
2019-08-13 01:21:49 -0400

    use fragments

--- src/util/mutations.ts
+++ src/util/mutations.ts
@@ -1,13 +1,12 @@
 import gql from 'graphql-tag';
 
+import { reservationFragment } from './queries'
+
 export const newReservation = gql`
   mutation CreateReservation($input: ReservationCreateInput!) {
     reservation: createReservation(data: $input) {
-      id
-      name
-      hotelName
-      arrivalDate
-      departureDate
+      ...ReservationDetail
     }
   }
+  ${reservationFragment}
 `;
--- src/util/queries.ts
+++ src/util/queries.ts
@@ -1,25 +1,29 @@
 import gql from 'graphql-tag';
 
+export const reservationFragment = gql`
+  fragment ReservationDetail on Reservation {
+    id
+    name
+    hotelName
+    arrivalDate
+    departureDate
+  }
+`;
+
 export const reservationQuery = gql`
   query ReservationQuery ($where: ReservationWhereUniqueInput!) {
     reservation(where: $where) {
-      id
-      name
-      hotelName
-      arrivalDate
-      departureDate
+      ...ReservationDetail
     }
   }
+  ${reservationFragment}
 `;
 
 export const loadReservationsQuery = gql`
   query loadReservations {
     reservations(first: 25, orderBy: createdAt_DESC) {
-      id
-      name
-      hotelName
-      arrivalDate
-      departureDate
+      ...ReservationDetail
     }
   }
+  ${reservationFragment}
 `;
